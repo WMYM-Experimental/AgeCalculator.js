074a700ce60642f38760f9b64c911fa44a3db469
WashingtonYandun <washington.yandun06@gmail.com>
2022-02-27 00:18:08 -0500

    start making calculateAge function and add an eventListener for the btn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("Hola mundo");
+const calculate = document.getElementById("send");
+
+const calculateAge = () => {
+  // date of bith
+  const dayOfBirth = document.getElementById("day");
+  const monthOfBirth = document.getElementById("month");
+  const yearOfBirth = document.getElementById("year");
+
+  // current date
+  const currentDate = new Date();
+
+  const currentDay = currentDate.getDate();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
+
+  console.log(currentDay, currentMonth, currentYear);
+};
+
+calculate.addEventListener("click", () => {
+  calculateAge();
+});
