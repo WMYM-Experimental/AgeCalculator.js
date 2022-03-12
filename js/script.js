cf0ca4d610bfa38a31d5be5e1b63f347d48e44b8
WashingtonYandun <washington.yandun06@gmail.com>
2022-03-11 23:55:54 -0500

    start making calculation proccess

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-// possible  months
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
 // GLOBAL VARIABLES
 const calc = document.getElementById("send");
 const input = document.getElementById("date-input");
+const months = [null, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const reset = () => {
   // reset values
@@ -20,6 +18,15 @@ const checkDateValidity = (birthDate, todayDate) => {
     return false;
   }
   return true;
+};
+
+const isLeapYear = (date) => {
+  if (date.leapYear) {
+    months[date.month] = 29;
+  } else {
+    months[date.month] = 28;
+  }
+  return false;
 };
 
 const calculateAge = () => {
@@ -42,8 +49,29 @@ const calculateAge = () => {
     leapYear: false,
   };
 
+  console.log(birth);
+  console.log(today);
+  console.log(validBirthDate);
+
   if (validBirthDate) {
     // start calculating
+    birth.year = today.year - birth.year;
+
+    isLeapYear(birth);
+
+    if (today.month >= birth.month) {
+      birth.month = today.month - birth.month;
+    } else {
+      birth.year = birth.year - 1;
+      birth.month = 12 + today.month - birth.month;
+    }
+
+    if (today.day >= birth.day) {
+      birth.day = today.day - birth.day;
+    } else {
+      birth.month = birth.month - 1;
+      birth.day = months[today.month - 1] + today.day - birth.day;
+    }
   } else {
     // reset if its not
     reset();
